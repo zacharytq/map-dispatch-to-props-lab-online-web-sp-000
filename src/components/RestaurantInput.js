3c284e5e983241cecc5ea95a146224c397a501bf
zacharytq <zacharytq@gmail.com>
2021-03-26 13:29:12 -0400

    commit tree 14d74896aac2f25a527fab28b31c104306f6a810

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -51,4 +52,4 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, { addRestaurant })(RestaurantInput);
